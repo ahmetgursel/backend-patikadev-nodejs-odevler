95d22c151023d14728550b6f40d531626345a02d
ahmetgursel <ahmetgursel@protonmail.com>
2021-09-07 23:34:49 +0300

    odev3 comp

---
+++ odev3/circle.js
@@ -0,0 +1,14 @@
+function circleArea(r) {
+  const pi = 3.14;
+  console.log(pi * r * r);
+}
+
+function circleCircumference(r) {
+  const pi = 3.14;
+  console.log(2 * pi * r);
+}
+
+module.exports = {
+  circleArea,
+  circleCircumference,
+};
