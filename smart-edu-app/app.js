9e0d790d3006b344a99fabef4b0b7949ad8f6427
ahmetgursel <ahmetgursel@protonmail.com>
2021-09-18 03:08:01 +0300

    course model added

--- smart-edu-app/app.js
+++ smart-edu-app/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 const pageController = require('./controllers/pageControllers');
 const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
+
 const app = express();
+
+//Connect to db
+mongoose.connect('mongodb://localhost:27017/smartedu-db').then(() => {
+  console.log('DB connected succesfully.');
+});
 
 //Template Engine
 app.set('view engine', 'ejs');
@@ -12,6 +20,7 @@ app.use(express.static('public'));
 
 //Routes
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 const port = 3000;
 app.listen(port, () => {
