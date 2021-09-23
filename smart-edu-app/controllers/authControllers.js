8b769af8a677d31633258d227792ef03f3c67b44
ahmetgursel <ahmetgursel@protonmail.com>
2021-09-23 23:27:20 +0300

    redirect & auth middleware

--- smart-edu-app/controllers/authControllers.js
+++ smart-edu-app/controllers/authControllers.js
@@ -26,7 +26,7 @@ exports.loginUser = (req, res) => {
         bcrypt.compare(password, user.password, (err, samePass) => {
           if (samePass) {
             req.session.userID = user._id;
-            res.status(200).redirect('/');
+            res.status(200).redirect('/users/dashboard');
           }
         });
       }
@@ -42,5 +42,13 @@ exports.loginUser = (req, res) => {
 exports.logoutUser = (req, res) => {
   req.session.destroy(() => {
     res.redirect('/');
+  });
+};
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({ _id: req.session.userID });
+  res.status(200).render('dashboard', {
+    page_name: 'dashboard',
+    user,
   });
 };
--- smart-edu-app/routes/userRoute.js
+++ smart-edu-app/routes/userRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const authController = require('../controllers/authControllers');
+const authMiddlaware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router.route('/signup').post(authController.createUser);
 router.route('/login').post(authController.loginUser);
 router.route('/logout').get(authController.logoutUser);
+router.route('/dashboard').get(authMiddlaware, authController.getDashboardPage);
 
 module.exports = router;
