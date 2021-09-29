bc2649fb9b678482a3a72c93831dff1ecc1554c4
ahmetgursel <ahmetgursel@protonmail.com>
2021-09-29 18:20:11 +0300

    contact form & mail service added

--- smart-edu-app/controllers/pageControllers.js
+++ smart-edu-app/controllers/pageControllers.js
@@ -1,3 +1,5 @@
+const nodemailer = require('nodemailer');
+
 exports.getIndexPage = (req, res) => {
   console.log(req.session.userID);
   res.status(200).render('index', {
@@ -21,4 +23,48 @@ exports.getLoginPage = (req, res) => {
   res.status(200).render('login', {
     page_name: 'login',
   });
+};
+
+exports.getContactPage = (req, res) => {
+  res.status(200).render('contact', {
+    page_name: 'contact',
+  });
+};
+
+exports.sendEmail = async (req, res) => {
+  const outputMessage = `
+  <h1>Mail Deteails </h1>
+  <ul>
+    <li>Name: ${req.body.name}</li>
+    <li>Email: ${req.body.email}</li>
+  </ul>
+  <h1>Message</h1>
+  <p>${req.body.message}</p>
+  `;
+
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: 'ahmt.gursel1515@gmail.com', // generated ethereal user
+      pass: 'vghijelepmfthviz', // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Smart EDU Contact Form" <ahmt.gursel1515@gmail.com>', // sender address
+    to: 'ahmt_gursel@hotmail.com', // list of receivers
+    subject: 'Smart EDU Contact Form New Message', // Subject line
+    html: outputMessage, // html body
+  });
+
+  console.log('Message sent: %s', info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+  res.status(200).redirect('/');
 };
