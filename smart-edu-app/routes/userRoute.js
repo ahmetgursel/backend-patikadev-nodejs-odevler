33279dac29a0168fa236089d067e20f13e18bc0b
ahmetgursel <ahmetgursel@protonmail.com>
2021-09-29 19:46:17 +0300

    added validator for register & login

--- smart-edu-app/routes/userRoute.js
+++ smart-edu-app/routes/userRoute.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const { body } = require('express-validator');
 const authController = require('../controllers/authControllers');
 const authMiddlaware = require('../middlewares/authMiddleware');
+const User = require('../models/User');
 
 const router = express.Router();
 
-router.route('/signup').post(authController.createUser);
+router.route('/signup').post(
+  [
+    body('name').not().isEmpty().withMessage('Please Enter Your Name'),
+    body('email')
+      .not()
+      .isEmpty()
+      .withMessage('Please Enter Valid Email')
+      .custom((userEmail) => {
+        return User.findOne({ email: userEmail }).then((user) => {
+          if (user) {
+            return Promise.reject('Email is already exists!');
+          }
+        });
+      }),
+    body('password').not().isEmpty().withMessage('Please Enter Your Password'),
+  ],
+  authController.createUser
+);
 router.route('/login').post(authController.loginUser);
 router.route('/logout').get(authController.logoutUser);
 router.route('/dashboard').get(authMiddlaware, authController.getDashboardPage);
